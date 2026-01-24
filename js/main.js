d047765043aded226c5a0d2720a6f8409c23e93e
Bunyod <zaripovbunyod571@gmail.com>
2026-01-24 16:18:18 +0500

    added mobil navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const menuBtn = document.querySelector("#menu-button");
+const navMobile = document.querySelector("#nav-mobile");
+const closeBtn = document.querySelector("#close-menu");
 
+menuBtn.addEventListener("click", () => {
+  navMobile.classList.remove("-right-300");
+  navMobile.classList.add("right-0");
+});
+
+closeBtn.addEventListener("click", () => {
+  navMobile.classList.remove("right-0");
+  navMobile.classList.add("-right-300");
+});
