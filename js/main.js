58576db607032a8828d3ad5bfd6f08eade78b2ce
Bunyod <zaripovbunyod571@gmail.com>
2026-02-16 00:05:11 +0500

    fixed hero & about sections

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 const menuBtn = document.querySelector("#menu-button");
 const navMobile = document.querySelector("#nav-mobile");
 const closeBtn = document.querySelector("#close-menu");
+const navLinks = document.querySelectorAll(".cl-modal");
 
 menuBtn.addEventListener("click", () => {
   navMobile.classList.remove("-right-300");
   navMobile.classList.add("right-0");
 });
 
-closeBtn.addEventListener("click", () => {
+function closeMenu() {
   navMobile.classList.remove("right-0");
   navMobile.classList.add("-right-300");
+}
+
+closeBtn.addEventListener("click", closeMenu);
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
 });
